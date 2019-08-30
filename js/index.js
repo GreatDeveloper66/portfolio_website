8afea7921b3fe2e06db0b913562074ab3f38b142
Adam <adam@mac.attlocal.net>
2019-08-30 17:15:23 -0500

    Added some scrolling functioning

--- js/index.js
+++ js/index.js
@@ -11,16 +11,21 @@
 window.onload = () => {
 	function scrollLoop() {
 	 let scrollRate = 0.4;
-		
 		let element = document.querySelector("div.headerbackground");
-		
 		const setTranslate = (xPos, yPos, el) => el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
-		
 	setTranslate(0, window.scrollY * scrollRate, element);
 		requestAnimationFrame(scrollLoop);
 	}
 window.addEventListener('scroll', function(e){
-	scrollLoop(0.35);
+	scrollLoop();
+	if(window.scrollY > 500){
+			document.querySelector("menu").style.position = "fixed";
+			document.querySelector(".mobilemenu").style.position = "fixed";
+	}
+	else {
+		document.querySelector("menu").style.position = "relative";
+			document.querySelector(".mobilemenu").style.position = "absolute";
+	}
 });
 
 };
