569ae66154f16d747219f2a347dbfaf930282b93
Adam <adam@mac.attlocal.net>
2019-08-30 15:52:55 -0500

    Finally got parallax effect to work for header image

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/*global window*/
+/*global document*/
+/*global Modernizr*/
+/*global SmoothScroll*/
+/*global console*/
+/*global $*/
+/*jshint esversion:6*/
+/*global event*/
+/*global requestAnimationFrame*/
+
+window.onload = () => {
+	function scrollLoop() {
+	 let scrollRate = 0.4;
+		
+		let element = document.querySelector("div.headerbackground");
+		
+		const setTranslate = (xPos, yPos, el) => el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+		
+	setTranslate(0, window.scrollY * scrollRate, element);
+		requestAnimationFrame(scrollLoop);
+	}
+window.addEventListener('scroll', function(e){
+	scrollLoop(0.35);
+});
+
+};
+
+
