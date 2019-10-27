6cd969f321160382bf5c25ab55b665cfc4e1b9a3
Adam <adam@mac.attlocal.net>
2019-10-27 18:32:57 -0500

    Change smooth scroll behavior to JQuery function

--- js/index.js
+++ js/index.js
@@ -10,18 +10,25 @@
 /*global CSS*/
 import toggleDropDown from '/js/toggleDropDown.js';
 import smoothScroll from '/js/scroll.js';
+import scrollJQuery from '/js/scrollJQuery.js';
 
 window.onload = () => {
 
+
+		scrollJQuery();
+	
 	document.getElementById("accordian").addEventListener("click", function (e) {
 		toggleDropDown(this, document.querySelector("nav"), "slidedown", "slideup");
 	});
 	Array.from(document.querySelectorAll("nav a")).forEach(elem => {
 		elem.addEventListener("click", function (e) {
 			toggleDropDown(this, document.querySelector("nav"), "slidedown", "slideup");
+			/*
    if(!CSS.supports("scroll-behavior","smooth")){
      smoothScroll(elem.getAttribute("href"));
+
    }
+	 */
 		});
 	});
 
