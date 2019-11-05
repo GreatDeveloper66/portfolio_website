9cd3e6413879a6425867e38480b55c18d104c112
Adam <adam@mac.attlocal.net>
2019-11-05 16:28:26 -0600

    Add parallax effect to Social Media background

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ import scrollJQuery from '/js/scrollJQuery.js';
 
 window.onload = () => {
   $('.header-window').parallax({imageSrc: '/images/Website_Background_2.jpg',speed: 0.4});
+	$('section#socialMedia').parallax({imageSrc: '/images/socialmedia.jpg', speed: 0.3});
 
 	scrollJQuery();
 	document.getElementById("accordian").addEventListener("click", function (e) {
